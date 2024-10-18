344a18752637b98278ee37554eb202f97ae5bbce
berrosss <167909033+wanesni@users.noreply.github.com>
2024-10-18 21:25:57 +0100

    update otp verification

--- extensions/otp_verification/src/index.ts
+++ extensions/otp_verification/src/index.ts
@@ -1,85 +1,85 @@
 import { defineEndpoint } from "@directus/extensions-sdk";
 
-export default defineEndpoint((router, context) => {
+export default defineEndpoint(async (router, context) => {
   const { services, getSchema } = context;
   const { UsersService, MailService } = services;
 
-  const usersService = new UsersService({ schema: getSchema });
-  const mailService = new MailService({ schema: getSchema });
+    const schema = await getSchema();
 
-  router.post("/", async (_req, res) => {
-    const { email, code } = _req.body;
+    const usersService = new UsersService({ schema });
+    const mailService = new MailService({ schema });
 
-    // Check if request body contains data
-    if (!email || !code) {
-      return res.status(400).send({
-        errors: [
-          {
-            message: "Payload email and code are required",
-            extensions: {
-              code: "BAD_REQUEST",
-            },
-          },
-        ],
-      });
-    }
+    router.post("/", async (_req, res) => {
+      const { email, code } = _req.body;
 
-    try {
-      // Check if the user already exists
-      const existingUser = await usersService.readByQuery({
-        filter: {
-          email: { _eq: email },
-        },
-      });
-
-      if (existingUser && existingUser.length > 0) {
-        return res.status(409).send({
+      if (!email || !code) {
+        return res.status(400).send({
           errors: [
             {
-              message: "Email already exists",
+              message: "Payload email and code are required",
               extensions: {
-                code: "CONFLICT",
+                code: "BAD_REQUEST",
               },
             },
           ],
         });
       }
 
-      // Send email
-      await mailService.send({
-        to: `${email}`,
-        subject: "WANESNI EMAIL VERIFICATION",
-        template: {
-          name: "otp-email-template",
-          data: {
-            code: `${code}`,
+      try {
+        // Check if the user already exists
+        const existingUser = await usersService.readByQuery({
+          filter: {
+            email: { _eq: email },
           },
-        },
-      });
+        });
 
-      return res.status(200).send({
-        data: [
-          {
-            message: `Email sent to ${email}`,
-            extensions: {
-              code: "SUCCESS",
+        if (existingUser && existingUser.length > 0) {
+          return res.status(409).send({
+            errors: [
+              {
+                message: "Email already exists",
+                extensions: {
+                  code: "CONFLICT",
+                },
+              },
+            ],
+          });
+        }
+
+        // Send email
+        await mailService.send({
+          to: `${email}`,
+          subject: "WANESNI EMAIL VERIFICATION",
+          template: {
+            name: "otp-email-template",
+            data: {
+              code: `${code}`,
             },
           },
-        ],
-      });
-    } catch (error) {
-      // Handle any unexpected errors
-      return res.status(500).send({
-        errors: [
-          {
-            message: error.message || "An unexpected error occurred",
-            extensions: {
-              code: "INTERNAL_SERVER_ERROR",
+        });
+
+        return res.status(200).send({
+          data: [
+            {
+              message: `Email sent to ${email}`,
+              extensions: {
+                code: "SUCCESS",
+              },
             },
-          },
-        ],
-      });
-    }
+          ],
+        });
+      } catch (error: any) {
+        return res.status(500).send({
+          errors: [
+            {
+              message: error.message || "An unexpected error occurred",
+              extensions: {
+                code: "INTERNAL_SERVER_ERROR",
+              },
+            },
+          ],
+        });
+      }
+    });
+
   });
-});
-

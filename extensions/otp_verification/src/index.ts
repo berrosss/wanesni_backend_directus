d80abc56eb7f818d3f43cd3c0b2fbd2ff888d03b
berrosss <167909033+wanesni@users.noreply.github.com>
2024-10-20 16:56:22 +0100

    Update index.ts

--- extensions/otp_verification/src/index.ts
+++ extensions/otp_verification/src/index.ts
@@ -1,7 +1,7 @@
 import { defineEndpoint } from "@directus/extensions-sdk";
 
-export default defineEndpoint(async (router, context) => {
-    router.post("/", async (_req, res) => {
+export default defineEndpoint((router, context) => {
+  router.post("/", async (_req: any, res: any) => {
       const { services, getSchema } = context;
       const { UsersService, MailService } = services;
       const schema = await getSchema();
@@ -10,74 +10,88 @@ export default defineEndpoint(async (router, context) => {
 
       const { email, code } = _req.body;
 
-      if (!email || !code) {
-        return res.status(400).send({
-          errors: [
-            {
-              message: "Payload email and code are required",
-              extensions: {
-                code: "BAD_REQUEST",
-              },
-            },
-          ],
-        });
-      }
+      console.log("email : ", email);
+      console.log("code : ", code);
 
-      try {
-        // Check if the user already exists
-        const existingUser = await usersService.readByQuery({
-          filter: {
-            email: { _eq: email },
-          },
-        });
-
-        if (existingUser && existingUser.length > 0) {
-          return res.status(409).send({
-            errors: [
-              {
-                message: "Email already exists",
-                extensions: {
-                  code: "CONFLICT",
-                },
-              },
-            ],
-          });
-        }
-
-        // Send email
-        await mailService.send({
-          to: `${email}`,
-          subject: "WANESNI EMAIL VERIFICATION",
-          template: {
-            name: "otp-email-template",
-            data: {
-              code: `${code}`,
+      return res.status(200).send({
+        data: [
+          {
+            message: `Email ${email}, Code ${code} `,
+            extensions: {
+              code: "SUCCESS",
             },
           },
-        });
+        ],
+      });
 
-        return res.status(200).send({
-          data: [
-            {
-              message: `Email sent to ${email}`,
-              extensions: {
-                code: "SUCCESS",
-              },
-            },
-          ],
-        });
-      } catch (error: any) {
-        return res.status(500).send({
-          errors: [
-            {
-              message: error.message || "An unexpected error occurred",
-              extensions: {
-                code: "INTERNAL_SERVER_ERROR",
-              },
-            },
-          ],
-        });
-      }
-    });
+      // if (!email || !code) {
+      //   return res.status(400).send({
+      //     errors: [
+      //       {
+      //         message: "Payload email and code are required",
+      //         extensions: {
+      //           code: "BAD_REQUEST",
+      //         },
+      //       },
+      //     ],
+      //   });
+      // }
+
+      // try {
+      //   const existingUser = await usersService.readByQuery({
+      //     filter: {
+      //       email: { _eq: email },
+      //     },
+      //   });
+
+      //   if (existingUser && existingUser.length > 0) {
+      //     return res.status(409).send({
+      //       errors: [
+      //         {
+      //           message: "Email already exists",
+      //           extensions: {
+      //             code: "CONFLICT",
+      //           },
+      //         },
+      //       ],
+      //     });
+      //   }
+
+      //   // Send email
+      //   await mailService.send({
+      //     to: `${email}`,
+      //     subject: "WANESNI EMAIL VERIFICATION",
+      //     template: {
+      //       name: "otp-email-template",
+      //       data: {
+      //         code: `${code}`,
+      //       },
+      //     },
+      //   });
+
+      //   return res.status(200).send({
+      //     data: [
+      //       {
+      //         message: `Email sent to ${email}`,
+      //         extensions: {
+      //           code: "SUCCESS",
+      //         },
+      //       },
+      //     ],
+      //   });
+      // } catch (error: any) {
+      //   return res.status(500).send({
+      //     errors: [
+      //       {
+      //         message: error.message || "An unexpected error occurred",
+      //         extensions: {
+      //           code: "INTERNAL_SERVER_ERROR",
+      //         },
+      //       },
+      //     ],
+      //   });
+      // }
 
   });
+
+});

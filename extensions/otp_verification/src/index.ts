f4310f3362e1e5b804752cc34fab423a8a363223
berrosss <167909033+wanesni@users.noreply.github.com>
2024-10-20 17:08:01 +0100

    Update index.ts

--- extensions/otp_verification/src/index.ts
+++ extensions/otp_verification/src/index.ts
@@ -2,11 +2,18 @@ import { defineEndpoint } from "@directus/extensions-sdk";
 
 export default defineEndpoint((router, context) => {
   router.post("/", async (_req: any, res: any) => {
-      // const { services, getSchema } = context;
-      // const { UsersService, MailService } = services;
-      // const schema = await getSchema();
-      // const usersService = new UsersService({ schema });
-      // const mailService = new MailService({ schema });
+      const { services, getSchema } = context;
+      const { UsersService, MailService } = services;
+      const schema = await getSchema();
+      const usersService = new UsersService({
+        schema,
+        accountability: {
+          ..._req.accountability,
+          admin: true,
+          role: "364b72a5-6d56-4039-aaa2-a5b5ac255784",
+        },
+      });
+      const mailService = new MailService({ schema });
 
       const { email, code } = _req.body;
 
